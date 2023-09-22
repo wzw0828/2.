a10f913a1386f6c673d6214a39a3009238d2ab99
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-09-22 10:52:43 +0800

    Create Ex04.java

---
+++ Ex04.java
@@ -0,0 +1,19 @@
+class CBox
+{
+    int length;
+    int width;
+    int height;
+}
+public class Ex04{
+    public static void main(String arge[])
+    {
+        CBox box;
+        box=new CBox();
+        box.length=15;
+        box.width=10;
+        box.height=25;
+        System.out.println("length= "+box.length);
+        System.out.println("width="+box.width);
+        System.out.println("height="+box.height);
+    }
+}
